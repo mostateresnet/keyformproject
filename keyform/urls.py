d4c614df41b80a5bb84132679ceca0e5845a3433
Rei Radford <reiozradford@gmail.com>
2017-02-28 14:09:51 -0600

    make a simple view for html

--- keyform/urls.py
+++ keyform/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from keyform import views 
 
 urlpatterns = [
     url(r'^$', views.HomeView.as_view())
--- keyform/views.py
+++ keyform/views.py
@@ -2,5 +2,7 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+class HomeView(TemplateView):
+	template_name = "keyform/home.html"
