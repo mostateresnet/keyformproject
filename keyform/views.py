3a487a19089a2415d781be1412bc9bd0995c2922
Patrick Ayres <theend7321@gmail.com>
2017-03-03 12:56:06 -0600

    fix the format of the date, and some other issues

--- keyform/views.py
+++ keyform/views.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
 from django.views.generic.list import ListView
-from keyform.models import Request, Comment
+from keyform.models import Request
 
 
 class HomeView(ListView):
     model = Request
     template_name = "keyform/home.html"
-    
