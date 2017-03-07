4b07d9cd8426eb09c81914d148fe7296a8f21750
Patrick Ayres <theend7321@gmail.com>
2017-03-07 14:20:08 -0600

    Really remove it

--- keyform/urls.py
+++ keyform/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from keyform import views
-from django.conf import settings
-import debug_toolbar
 
 urlpatterns = [
     url(r'^$', views.HomeView.as_view()),
