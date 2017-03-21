e5b80dab18dc9f7444667d271415444c2750751f
Rei Radford <reiozradford@gmail.com>
2017-03-21 12:14:51 -0500

    added new lines and removed irrelevant code

--- keyform/urls.py
+++ keyform/urls.py
@@ -3,6 +3,5 @@ from django.contrib import admin
 from keyform import views 
 
 urlpatterns = [
-    url(r'^$', views.HomeView.as_view()),
     url(r'^create_new', views.NewForm.as_view(), name='NewForm'),
 ]
--- keyform/views.py
+++ keyform/views.py
@@ -8,9 +8,3 @@ from django.views.generic import CreateView
 
 class HomeView(TemplateView):
 	template_name = "keyform/home.html"
-
-class NewForm(CreateView):
-	template_name = "keyform/add_form.html"
-	model = Request
-	fields = ['building', 'student_name', 'reason_for_request', 'amt_recieved', 'payment_method', 'charge_amount', 'staff', 'bpn', 'created_timestamp', 'charged_on_rcr']
-	success_url = "/"
