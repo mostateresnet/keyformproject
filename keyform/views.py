8eabfbc95959161b4b3be0c99ef9971e3004b1e2
Rei Radford <reiozradford@gmail.com>
2017-03-28 11:18:27 -0500

    remove unnecessary imports

--- keyform/views.py
+++ keyform/views.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
-from django.views.generic.base import TemplateView
 from django.views.generic import CreateView
 from django.views.generic.list import ListView
 from keyform.forms import CreateForm
