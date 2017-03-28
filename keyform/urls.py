e42503d2184b8cf4b29007d08da2522ad0d2e228
Patrick Ayres <theend7321@gmail.com>
2017-03-28 09:58:48 -0500

    Start on clickable requests

--- keyform/urls.py
+++ keyform/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 from keyform import views
 
 urlpatterns = [
-    url(r'^$', views.HomeView.as_view()),
+    url(r'^$', views.HomeView.as_view(), name = 'home'),
+    url(r'^request/(?P<pk>\d+)$', views.RequestView.as_view(), name = 'request'),
 ]
--- keyform/views.py
+++ keyform/views.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.views.generic.list import ListView
+from django.views.generic import ListView, UpdateView
 from keyform.models import Request
 
 
 class HomeView(ListView):
     model = Request
     template_name = "keyform/home.html"
+
+class RequestView(UpdateView):
+    model = Request
+    template_name = "keyform/request.html"
+    fields = ['student_name']
