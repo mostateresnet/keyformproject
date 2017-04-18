ce162f76323336f0da6370e5c0abca59a53c2ab7
Rei Radford <reiozradford@gmail.com>
2017-04-18 15:23:04 -0500

    make the "Add Field" button create extra key data fields

---
+++ keyform/static/keyform/js/libraries/add_form.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('#generate_forms').click(function() {
+        var form_idx = $('#id_keydata_set-TOTAL_FORMS').val();
+        $('#form_set').append($('#empty_form').html().replace(/__prefix__/g, form_idx));
+        $('#id_keydata_set-TOTAL_FORMS').val(parseInt(form_idx) + 1);
+    });
+});
