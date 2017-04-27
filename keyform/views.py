8ab9287bbdb0cd87eb4ec8e2f522103bc511263d
Rei Radford <reiozradford@gmail.com>
2017-04-27 14:06:50 -0500

    add a delete button for keydata forms and turn the function into a class-based view

--- keyform/views.py
+++ keyform/views.py
@@ -1,33 +1,36 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.views.generic import CreateView
+from django.views.generic import FormView
 from django.views.generic.list import ListView
 from keyform.forms import CreateForm, RequestFormSet
 from keyform.models import Request
 from django.urls import reverse_lazy
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from django.core.urlresolvers import reverse
+from django.core.urlresolvers import reverse_lazy
 
 class HomeView(ListView):
     model = Request
     template_name = "keyform/home.html"
 
-def key_request(request):
-    if request.POST:
-        form = CreateForm(request.POST)
-        if form.is_valid():
-            create_new = form.save(commit=False)
-            key_formset = RequestFormSet(request.POST, instance=create_new)
-            if key_formset.is_valid():
-                create_new.save()
-                key_formset.save()
-                return HttpResponseRedirect(reverse('home'))
-    else:
-        form = CreateForm()
-        key_formset = RequestFormSet(instance=Request())
-    return render(request, "keyform/add_form.html", {
-        "form": form,
-        "keydata_formset": key_formset,
-    })
+class KeyRequest(FormView):
+    template_name = "keyform/add_form.html"
+    success_url = reverse_lazy("home")
+
+    def form_valid(self, form):
+        new_request = form.save(commit=False)
+        form.request_formset = RequestFormSet(self.request.POST, instance=new_request)
+        for request_form in form.request_formset.forms:
+            request_form.empty_permitted = False
+        if form.request_formset.is_valid() and form.request_formset.has_changed():
+            new_request.save()
+            form.request_formset.save()
+            return HttpResponseRedirect(self.get_success_url())
+        else:
+            return self.form_invalid(form)
+
+    def get_form(self, form_class=None):
+        form = CreateForm(**self.get_form_kwargs())
+        form.request_formset = RequestFormSet(**self.get_form_kwargs())
+        return form
