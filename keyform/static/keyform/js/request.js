a9aff3dfb3821eeb0821837104bfdaf74804ff08
Shane Allgeier <shaneallgeier@gmail.com>
2017-06-22 16:04:37 -0500

    Change comment form logic

--- keyform/static/keyform/js/request.js
+++ keyform/static/keyform/js/request.js
@@ -1,27 +1,17 @@
 $(document).ready(function() {
-    $('#cancel').css("display", 'none');
-    $('#submit').css("display", 'none')
-    var div = document.getElementById('comments');
     $('#add').click(function() {
-        $('#add').css("display", 'none');
-        $('#cancel').css("display", 'inline-block');
-        $('#submit').css("display", 'inline-block')
-        div.innerHTML = '<p>Comment:</p><textarea id="comment-text" cols="40" rows="4"></textarea>';
+        $('#comments').toggleClass('add-comment', true);
     });
 
     $('#cancel').click(function() {
-        $('#add').css("display", 'inline-block');
-        $('#cancel').css("display", 'none');
-        $('#submit').css("display", 'none');
-        div.innerHTML = '';
+        $('#comments').toggleClass('add-comment', false);
     });
 
     $('#submit').click(function() {
         if(confirm("Are you sure you want to add this comment?")) {
             var message = $('#comment-text').val();
             var pk = $('#key-section').data('pk');
-            console.log(message);
-            $.post('/add-comment', { message:message, pk:pk }, function() {
+            $.post(COMMENT_URL, { message:message, pk:pk }, function() {
                 location.reload();
             });
         }
