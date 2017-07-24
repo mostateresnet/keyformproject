77806131ae868a67856cdeb6bb10c3e07c3b7f2a
Rei Radford <reiozradford@gmail.com>
2017-07-24 17:14:33 -0500

    unbroke "everything"

--- keyform/static/keyform/js/add_form.js
+++ keyform/static/keyform/js/add_form.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $('#form-set').on('click', '.generate_forms', function() {
         var form_idx = $('#id_keydata_set-TOTAL_FORMS').val();
         $('#form-set').append($('#empty_form').html().replace(/__prefix__/g, form_idx));
-        
+        $('#id_keydata_set-TOTAL_FORMS').val(parseInt(form_idx) + 1);
     });
 
     $('#form-set').on('click', '.delete_button', function() {
