dd57535ac52650128ce08a87fd17bd039e93d95e
Rei Radford <reiozradford@gmail.com>
2018-03-27 15:34:53 -0500

    synchronize room number across the create form

--- keyform/static/keyform/js/add_form.js
+++ keyform/static/keyform/js/add_form.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
     $('#form-set').on('click', '.generate_forms', function() {
         var form_idx = $('#id_keydata_set-TOTAL_FORMS').val();
-        $('#form-set').append($('#empty_form').html().replace(/__prefix__/g, form_idx));
+        var new_form = $($('#empty_form').html().replace(/__prefix__/g, form_idx));
+        new_form.find('input[id$=-room_number]').val(previous_val);
+        $('#form-set').append(new_form);
         $('#id_keydata_set-TOTAL_FORMS').val(parseInt(form_idx) + 1);
     });
 
@@ -22,4 +24,18 @@ $(document).ready(function() {
     }
 
     $('#form-set').on('click change','.keydata-form select[data-pks_with_hide_core_number]', hide_keydata_field);
+
+    var global_room_number = $("[name=keydata_set-__prefix__-room_number]");
+    global_room_number.on('keyup', synchronize_room_number)
+
+    var previous_val = global_room_number.val();
+    function synchronize_room_number (){
+        var current_val = global_room_number.val();
+        $(".keydata-form input[id$=-room_number]").each(function() {
+            if ($(this).val() == previous_val){
+                $(this).val(current_val);
+            }
+        });
+        previous_val = current_val;
+    };
 });
