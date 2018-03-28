182a78688148509b47f080729cfe537c1c883445
Rei Radford <reiozradford@gmail.com>
2018-03-28 12:53:04 -0500

    synchronize room numbers in key data forms after refresh

--- keyform/static/keyform/js/add_form.js
+++ keyform/static/keyform/js/add_form.js
@@ -2,12 +2,12 @@ $(document).ready(function() {
     $('#form-set').on('click', '.generate_forms', function() {
         var form_idx = $('#id_keydata_set-TOTAL_FORMS').val();
         var new_form = $($('#empty_form').html().replace(/__prefix__/g, form_idx));
-        new_form.find('input[id$=-room_number]').val(previous_val);
+        new_form.find('input[id$=-room_number]').val(previous_room);
         $('#form-set').append(new_form);
         $('#id_keydata_set-TOTAL_FORMS').val(parseInt(form_idx) + 1);
     });
 
-    $('#form-set').on('click', '.delete_button', function() {
+    $('#form-set').on('click', '.delete_button', function(){
         if ($('#form-set .keydata-form').length > 1){
             var form_idx = $('#id_keydata_set-TOTAL_FORMS').val();
             $(this).closest('.keydata-form').remove();
@@ -15,7 +15,7 @@ $(document).ready(function() {
         }
     });
 
-    function hide_keydata_field() {
+    function hide_keydata_field(){
         var key_type_selector = $(this);
         var pks_with_hide_core_number = key_type_selector.data('pks_with_hide_core_number');
         pks_with_hide_core_number = String(pks_with_hide_core_number).split(',');
@@ -25,17 +25,33 @@ $(document).ready(function() {
 
     $('#form-set').on('click change','.keydata-form select[data-pks_with_hide_core_number]', hide_keydata_field);
 
-    var global_room_number = $("[name=keydata_set-__prefix__-room_number]");
-    global_room_number.on('keyup', synchronize_room_number)
+    function mode(arr) {
+        return arr.sort(function(a,b){
+            return arr.filter(function(v){ return v===a }).length
+                 - arr.filter(function(v){ return v===b }).length
+        }).pop();
+    }
 
-    var previous_val = global_room_number.val();
-    function synchronize_room_number (){
+    var room_list = [];
+    $(".keydata-form input[id$=-room_number]").each(function() {
+        room_list.push($(this).val());
+    });
+
+
+    var global_room_number = $("[name=keydata_set-__prefix__-room_number]");
+    var previous_room = mode(room_list);
+    global_room_number.val(previous_room);
+
+    function synchronize_room_number(){
         var current_val = global_room_number.val();
         $(".keydata-form input[id$=-room_number]").each(function() {
-            if ($(this).val() == previous_val){
+            if ($(this).val() == previous_room){
                 $(this).val(current_val);
             }
         });
-        previous_val = current_val;
+        previous_room = current_val;
     };
+
+    global_room_number.on('keyup', synchronize_room_number);
+
 });
